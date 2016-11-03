accff7ded035e1227c7b25b3930b061622ca3ffc
Ben Woodfield <raser.13@hotmail.com>
2016-11-03 19:49:07 +0000

    added to the final calculator app

--- python_lesson_feet-meters-calculator.py
+++ python_lesson_feet-meters-calculator.py
@@ -1,41 +1,38 @@
-# Ben Woodfield
-# Feet_2_Meters calculator.
-## Python Programming Community - GUI Lessons
+#!/usr/bin/env/python
+# Ben Woodfield - Python GUI - Development Lessons
+# Final App - Feet to Meters Calculator
 
-#!/usr/bin/env python   
-from Tkinter import *  
+from Tkinter import *
 
 top = Tk()
-top.minsize(400, 200)
-top.title('Your Name Here')
-
-lbl_one = Label(top, text='FEET to METERS Conversion Calculator', fg='purple', font='freesansBold, 14')
-lbl_one.pack()
-
-q = Button(top, text='Exit', command=quit, fg='red') 
-q.pack(side=BOTTOM, fill=X) 
+top.minsize(450,350)
+top.title('Feet To Meters Calculator App')
+top.configure(bg='DarkGrey')
 
 cvt_from = StringVar()
-cvt_to = StringVar() 
+cvt_to = StringVar()
 
-def do_convert():
+def calculate_feet_meter():
     feet_val = float(cvt_from.get())
-    meters_val = feet_val * 0.3048
-    cvt_to.set('%s meters' % meters_val)
+    meters_val = feet_val * 0.3048  # Full wave loop meters calculation
+    cvt_to.set('%s Meters ' % meters_val)
+    
+lbl_info = Label(top, text='Enter your value in Feet', fg='blue', bg='DarkGrey', font='freesansbold,16') 
+lbl_info.pack()
 
-from_lbl = Label(top, text='Enter FEET (numbers only):', fg='blue', font='12') 
-from_lbl.pack() 
+feet_input = Entry(top, font='freesansbold, 14', relief='raised', textvariable=cvt_from)
+feet_input.pack()
 
-from_entry = Entry(top, textvariable=cvt_from) 
-from_entry.pack() 
+lbl_2 = Label(top,text='Click "Calculate" to get results', fg='blue', bg='DarkGrey', font='freesansbold,16')
+lbl_2.pack()
 
-to_lbl = Label(top, text='Result:', fg='purple', font='12') 
-to_lbl.pack()
-
-result_lbl = Label(top, textvariable=cvt_to, bg='yellow', font='freesansBold, 12')
-result_lbl.pack()
-
-convert_btn = Button(top, text='Convert to Meters', fg='blue', command=do_convert)
+convert_btn = Button(top, text='Convert Feet to Meters', fg='blue', font='freesansbold, 14', command=calculate_feet_meter)
 convert_btn.pack()
+
+lbl_result = Label(top, textvariable=cvt_to, relief='ridge', font='freesansbold, 14', bg='Grey', fg='Blue')
+lbl_result.pack(fill=BOTH, expand=1)
+
+q = Button(top, text='Exit', command=quit, bg='DarkGrey', fg='red', font='freesansbold, 14') 
+q.pack(side=BOTTOM, fill=X)
 
 top.mainloop()
