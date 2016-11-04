1bde3ff23e9e1b6e562075cb44b63962475634f9
Ben Woodfield <raser.13@hotmail.com>
2016-11-04 14:41:19 +0000

    Add files via upload

---
+++ python_lesson_4.py
@@ -0,0 +1,20 @@
+# Lesson 4
+# A basic GUI program
+
+from Tkinter import *
+
+# Create the main window for our program
+top = Tk()
+top.minsize(300,300) # 300 x 300 is our x and y sizes
+top.title('Feet to Meters Calculator') # This is our title
+top.configure(bg='DarkGrey') # This is the background color
+
+lbl_one = Label (top, text='Enter your Value in Feet Below')
+lbl_one.pack()
+
+# Give your button a name
+btn_exit = Button(top, text='Exit', command=quit)
+btn_exit.pack(side=BOTTOM)
+
+# To make Python Display the window, run a main loop:
+top.mainloop()
